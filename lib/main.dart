71177d35a881a7ee25c9015a9419e5ddfc03a07e
Максим Заїка <888MAXIMUS888@i.ua>
2020-03-23 13:49:48 +0200

    "chaged test"

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter CI/CD Demo'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to Flutter CI/CD Demo'),
       ),
-    );
+      body: RaisedButton(
+        padding: EdgeInsets.all(20),
+        onPressed: () {
+          print("Raised Button is pressed");
+        },
+        child: Text("Press Me!!"),
+      ),
+    ));
   }
 }
